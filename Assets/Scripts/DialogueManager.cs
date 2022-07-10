6edda443a5674157659954bde9f2cc59b4cbbb54
Sounak Ghosh <ss3448@scarletmail.rutgers.edu>
2022-07-10 17:06:21 -0500

    applied dialgue system to trainingscene. gonna start working on trigger with vive controller and eye tracking

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -9,7 +9,7 @@ public class DialogueManager : MonoBehaviour {
 
     bool isTalking = false;
 
-    float distance;
+    //float distance;
     float curResponsetracker = 0f;
 
     public GameObject player;
@@ -26,42 +26,23 @@ public class DialogueManager : MonoBehaviour {
         dialogueUI.SetActive(false);
     }
 
-    void OnMouseOver()
+    void Update()
     {
-        distance = Vector3.Distance(player.transform.position, this.transform.position);
-        if(distance <= 2.5)
-        {
-            if(Input.GetAxis("Mouse ScrollWheel") < 0f)
-            {
-                curResponsetracker++;
-                if(curResponsetracker >= npc.playerDialogue.Length - 1)
-                {
-                    curResponsetracker = npc.playerDialogue.Length - 1;
-                }
-            }
-            else if(Input.GetAxis("Mouse ScrollWheel") > 0f)
-            {
-                curResponsetracker--;
-                if (curResponsetracker < 0f)
-                { 
-                    curResponsetracker = 0f;
-                }
-            }
             //trigger dialogue system
             if (Input.GetKeyDown(KeyCode.E) && isTalking == false)
             {
                 StartConversation();
-                
+
             }
             else if (Input.GetKeyDown(KeyCode.E) && isTalking == true)
             {
                 EndDialogue();
             }
 
-            if(curResponsetracker == 0 && npc.playerDialogue.Length >= 0)
+            if (curResponsetracker == 0 && npc.playerDialogue.Length >= 0)
             {
                 playerResponse.text = npc.playerDialogue[0];
-                if(Input.GetKeyDown(KeyCode.Space))
+                if (Input.GetKeyDown(KeyCode.Space))
                 {
                     npcDialogueBox.text = npc.dialogue[1];
                 }
@@ -82,8 +63,67 @@ public class DialogueManager : MonoBehaviour {
                     npcDialogueBox.text = npc.dialogue[3];
                 }
             }
-        }
     }
+
+    //void OnMouseOver()
+    //{
+    //    //distance = Vector3.Distance(player.transform.position, this.transform.position);
+    //    //if(Input.GetKeyDown(KeyCode.W))
+    //    //{
+    //        //if(Input.GetAxis("Mouse ScrollWheel") < 0f)
+    //        //{
+    //        //    curResponsetracker++;
+    //        //    if(curResponsetracker >= npc.playerDialogue.Length - 1)
+    //        //    {
+    //        //        curResponsetracker = npc.playerDialogue.Length - 1;
+    //        //    }
+    //        //}
+    //        //else if(Input.GetAxis("Mouse ScrollWheel") > 0f)
+    //        //{
+    //        //    curResponsetracker--;
+    //        //    if (curResponsetracker < 0f)
+    //        //    { 
+    //        //        curResponsetracker = 0f;
+    //        //    }
+    //        //}
+
+    //        //trigger dialogue system
+    //        if (Input.GetKeyDown(KeyCode.E) && isTalking == false)
+    //        {
+    //            StartConversation();
+                
+    //        }
+    //        else if (Input.GetKeyDown(KeyCode.E) && isTalking == true)
+    //        {
+    //            EndDialogue();
+    //        }
+
+    //        if(curResponsetracker == 0 && npc.playerDialogue.Length >= 0)
+    //        {
+    //            playerResponse.text = npc.playerDialogue[0];
+    //            if(Input.GetKeyDown(KeyCode.Space))
+    //            {
+    //                npcDialogueBox.text = npc.dialogue[1];
+    //            }
+    //        }
+    //        else if (curResponsetracker == 1 && npc.playerDialogue.Length >= 1)
+    //        {
+    //            playerResponse.text = npc.playerDialogue[1];
+    //            if (Input.GetKeyDown(KeyCode.Space))
+    //            {
+    //                npcDialogueBox.text = npc.dialogue[2];
+    //            }
+    //        }
+    //        else if (curResponsetracker == 2 && npc.playerDialogue.Length >= 2)
+    //        {
+    //            playerResponse.text = npc.playerDialogue[2];
+    //            if (Input.GetKeyDown(KeyCode.Space))
+    //            {
+    //                npcDialogueBox.text = npc.dialogue[3];
+    //            }
+    //        }
+    //    //}
+    //}
 
     void StartConversation()
     {
@@ -92,7 +132,6 @@ public class DialogueManager : MonoBehaviour {
         dialogueUI.SetActive(true);
         npcName.text = npc.name;
         npcDialogueBox.text = npc.dialogue[0];
-        Debug.Log(distance);
     }
 
     void EndDialogue()
